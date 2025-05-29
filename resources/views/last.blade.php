0cb751aae4762a6e186ba2ff73144ab3c1e6cb35
JoseJ <alvarezreyes246@gmail.com>
2025-05-28 21:50:19 -0500

    Fix: template

--- resources/views/last.blade.php
+++ resources/views/last.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="{{ secure_asset('index.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('index.css') }}" defer>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   </head>
   <body>
@@ -16,14 +16,14 @@
         <div class="sun"></div>
         <!-- cohete -->
         <div class="iconContainer">
-          <img style="width: 70%; height: 70%" class="icon" src="{{ secure_asset('rocket.svg') }}" />
+          <img style="width: 70%; height: 70%" class="icon" src="{{ secure_asset('rocket.svg') }}" defer/>
         </div>
         <!-- otros iconos -->
         <div>
-          <img class="iconElement" src="{{ secure_asset('braces-asterisk.svg') }}" />
-          <img class="iconElement" src="{{ secure_asset('database-fill.svg') }}" />
-          <img class="iconElement" src="{{ secure_asset('palette-fill.svg') }}" />
-          <img class="iconElement" src="{{ secure_asset('person-vcard-fill.svg') }}" />
+          <img class="iconElement" src="{{ secure_asset('braces-asterisk.svg') }}" defer/>
+          <img class="iconElement" src="{{ secure_asset('database-fill.svg') }}" defer/>
+          <img class="iconElement" src="{{ secure_asset('palette-fill.svg') }}" defer/>
+          <img class="iconElement" src="{{ secure_asset('person-vcard-fill.svg') }}" defer/>
         </div>
         <div>
            <svg class="containerLines" height="100%" width="100%">
@@ -45,6 +45,5 @@
        <h1 style="font-size: 48px; text-align: center; font-weight: 500px;">Fireploy App</h1>
       <h2 style="text-align: center;">Time to work! 😎</h2>
     </div>
-    <script src="/js/index.js"></script>
   </body>
 </html>
